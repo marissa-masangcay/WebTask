bc53affe4314a5c67830fb393897f5dad91897d7
Marissa Masangcay <mjmasangcay@usfca.edu>
2018-04-17 23:34:37 -0700

    Updating express-with-view.js

--- express-with-view.js
+++ express-with-view.js
@@ -59,10 +59,11 @@ app.post('/', upload.single('image'), (req, res) => {
     }));
   }, { /*categorization: 'imagga_tagging',
        auto_tagging: 0.4,*/
-       eager: {
-        // width: 150, height: 100, crop: "thumb", gravity: "face"}
-        width: 150, height: 100, crop: "thumb"}
-  });
+       eager: [
+        // {width: 150, height: 100, crop: "thumb", gravity: "face"},
+        {width: 150, height: 100, crop: "thumb"},
+        {width: 400, height: 300, quality: 'auto', format: 'auto'}
+     ]});
 });
 
 //Original View
